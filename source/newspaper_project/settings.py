3be41e6909e22ac03e45e76514ae068ca446f429
ElipLam <ltda2501@gmail.com>
2024-01-09 23:04:39 +0700

    custom user authentication

--- source/newspaper_project/settings.py
+++ source/newspaper_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "newspaper_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [Path(BASE_DIR, "templates")],  # add custom templates
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,3 +123,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.CustomUser"  # custom user model
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
