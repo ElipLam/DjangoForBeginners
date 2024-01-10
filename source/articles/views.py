e321136148e352053fc0c03d642a7e34edcff727
ElipLam <ltda2501@gmail.com>
2024-01-11 02:11:21 +0700

    add create  article

--- source/articles/views.py
+++ source/articles/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Article
 
@@ -28,3 +28,13 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        "title",
+        "body",
+        "author",
+    )
